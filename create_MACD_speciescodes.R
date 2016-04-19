34a62c768bedd136fb290e17d5ec044061c33795
Morgan Ernest <morgan@weecology.org>
2016-04-19 09:34:54 -0400

    Added function making unique MACD species table

--- create_MACD_speciescodes.R
+++ create_MACD_speciescodes.R
@@ -7,6 +7,18 @@ query_MACD = function(query){
   query_output <- dbGetQuery(conn, query)
 }
 
-genus_species_col = "SELECT genus, species FROM community_analysis_data"
-all_species_names = query_MACD(genus_species_col)
+make_species_table_from_MACD = function(){
+  genus_species_col = "SELECT DISTINCT family, genus, species FROM community_analysis_data"
+  unique_species = query_MACD(genus_species_col)
+  
+  number_species = nrow(unique_species)
+  species_codes = c(1:number_species)
+  unique_species = cbind(unique_species, species_codes)
+  return(unique_species)
+}
+
+MACD_species = make_species_table_from_MACD()
+
+
+
 
