6df5a9f4750cfb4c54322b92810e42bcf9dc184a
Morgan Ernest <morgan@weecology.org>
2016-04-19 12:47:25 -0400

    Add SQL queries to clean white space in species & genus fields

--- fix_MACD_names.R
+++ fix_MACD_names.R
@@ -9,6 +9,10 @@ query_MACD = function(query){
   query_output <- dbGetQuery(conn, query)
 }
 
-query1 = "UPDATE community_analysis_data SET species='cooperii' WHERE 
+query1 = "update community_analysis_data set species = LTRIM(RTRIM(species))"
+query2 = "update community_analysis_data set genus = LTRIM(RTRIM(genus))"
+query3= "UPDATE community_analysis_data SET species='cooperii' WHERE 
 species= 'cooperi'"
 query_MACD(query1)
+query_MACD(query2)
+query_MACD(query3)
