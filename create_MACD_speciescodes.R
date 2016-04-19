9e005738c314ceaa52cd4de8804606f35b53c6f7
Morgan Ernest <morgan@weecology.org>
2016-04-19 12:20:49 -0400

    Import & subset AMNIOTE database, match with MACD

--- create_MACD_speciescodes.R
+++ create_MACD_speciescodes.R
@@ -11,21 +11,23 @@ query_MACD = function(query){
 make_species_table_from_MACD = function(){
   genus_species_col = "SELECT DISTINCT family, genus, species FROM community_analysis_data"
   unique_species = query_MACD(genus_species_col)
-  
+  clean_whitespace = as.data.frame(apply(unique_species,2,function(x)gsub('\\s+', '',x)),
+                                   stringsAsFactors=FALSE)
   number_species = nrow(unique_species)
   species_codes = c(1:number_species)
   unique_species = cbind(unique_species, species_codes)
   return(unique_species)
 }
 
+make_AMNIOTE_species_table = function(){
+  AMNIOTE_DB = read.csv("Data/Amniote_Database_Aug_2015.csv", stringsAsFactors = FALSE)
+  AMNIOTE_DB = AMNIOTE_DB %>% select(genus, species, adult_body_mass_g)
+  AMNIOTE_DB = filter(AMNIOTE_DB, adult_body_mass_g > 0)
+  clean_AMNIOTE = as.data.frame(apply(AMNIOTE_DB,2,function(x)gsub('\\s+', '',x)), stringsAsFactors = FALSE)
+  }
+
 MACD_species = make_species_table_from_MACD()
+AMNIOTE_species = make_AMNIOTE_species_table()
 
-AMNIOTE_DB = read.csv("Data/Amniote_Database_Aug_2015.csv")
-AMNIOTE_DB[AMNIOTE_DB == -999] <- NA
-AMNIOTE_DB = filter(AMNIOTE_DB, adult_body_mass_g > 0)
-
-MACD_AMNIOTE_common_rows <- (AMNIOTE_DB$genus %in% MACD_species$genus 
-                                & AMNIOTE_DB$species %in% MACD_species$species)
-MACD_AMNIOTE_common_species <- AMNIOTE_DB[MACD_AMNIOTE_common_rows,]
-
+MACD_AMNIOTE_wgt = left_join(MACD_species, AMNIOTE_species, by = c("genus","species"))
 
