638d7b528fddeb2a624d627bce0cace07791c7a2
Morgan Ernest <morgan@weecology.org>
2016-05-06 12:39:03 -0400

    Created function to merge AMNIOTE and MACD species tables

--- create_MACD_speciescodes.R
+++ create_MACD_speciescodes.R
@@ -22,15 +22,24 @@ make_species_table_from_MACD = function(){
 
 make_AMNIOTE_species_table = function(){
   AMNIOTE_DB = read.csv("Data/Amniote_Database_Aug_2015.csv", stringsAsFactors = FALSE)
-  AMNIOTE_DB = AMNIOTE_DB %>% select(genus, species, adult_body_mass_g)
+  AMNIOTE_DB = AMNIOTE_DB %>% select(class, family, genus, species, adult_body_mass_g)
   AMNIOTE_DB = filter(AMNIOTE_DB, adult_body_mass_g > 0)
   clean_AMNIOTE = as.data.frame(apply(AMNIOTE_DB,2,function(x)gsub('\\s+', '',x)), stringsAsFactors = FALSE)
-  }
+}
+
+merge_AMNIOTE_MACD = function(AMNIOTE, MACD){
+  unique_species_wgt = left_join(MACD, AMNIOTE, by = c("genus","species"))
+  unique_species_wgt = unique_species_wgt[c(5,6,1,2,3,4,7)]
+  return(unique_species_wgt)
+}
 
 MACD_species = make_species_table_from_MACD()
 AMNIOTE_species = make_AMNIOTE_species_table()
-
-
+MACD_AMNIOTE_rawmerge = merge_AMNIOTE_MACD(AMNIOTE_species, MACD_species)
 
 unique_species_wgt = left_join(MACD_species, AMNIOTE_species, by = c("genus","species"))
+unique_species_wgt = unique_species_wgt[c(5,6,1,2,3,4,7)]
 
+for (row in 1:nrow(changes.file)) {
+  
+if is.na(unique_species_wgt$family.x)
